db2938e148911bc49ca78df0d8f6b2daa96620bb
Tanmay Naik <tnaik96@gmail.com>
2020-03-20 21:06:01 -0400

    added course css

--- src/app/services/CourseServiceClient.ts
+++ src/app/services/CourseServiceClient.ts
@@ -2,9 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CourseServiceClient {
-    public findAllCourses = () => {
-        console.log('here');
-        return fetch('http://wbdv-generic-server.herokuapp.com/api/tsnaik/courses')
-            .then((response) => response.json());
-    }
+    public findAllCourses = () =>
+        fetch('http://wbdv-generic-server.herokuapp.com/api/tsnaik/courses')
+            .then((response) => response.json())
 }
