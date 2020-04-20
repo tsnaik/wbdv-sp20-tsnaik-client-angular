a333b5c3fce9fc0c67634b7f58a13f2f0dbd23f1
Tanmay Naik <tnaik96@gmail.com>
2020-04-20 02:38:51 -0400

    change service url to heroku

--- src/app/services/course.service.client.ts
+++ src/app/services/course.service.client.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CourseServiceClient {
-    // private URL = 'https://wbdv-generic-server.herokuapp.com/api/tsnaik';
-    private URL = 'http://localhost:3000/api';
+    private URL = 'https://wbdv-generic-server.herokuapp.com/api/tsnaik';
+    // private URL = 'http://localhost:3000/api';
 
     public findAllCourses = () =>
         fetch(this.URL + '/courses')
--- src/app/services/question.service.client.ts
+++ src/app/services/question.service.client.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class QuestionServiceClient {
-    private URL = 'http://localhost:3000/api/quizzes';
-    // private URL = 'https://wbdv-sp20-tsnaik-server-node.herokuapp.com/api/quizzes';
+    // private URL = 'http://localhost:3000/api/quizzes';
+    private URL = 'https://wbdv-sp20-tsnaik-server-node.herokuapp.com/api/quizzes';
     findQuestionsForQuiz = (qid) =>
         fetch(`${this.URL}/${qid}/questions`)
             .then(response => response.json())
